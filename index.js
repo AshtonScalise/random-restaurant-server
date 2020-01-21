8a643e7c5a993af35cd2af3179114878f182857e
Frank Scalise <ashtonscalise@gmail.com>
2020-01-21 00:55:39 -0600

    added post request for front end to send zip

--- index.js
+++ index.js
@@ -1,57 +1,62 @@
-const stdin = process.openStdin()
-const express = require("express")
-const axios = require("axios")
-const zipcodes = require("zipcodes")
-const { token } = require('./api')
+const stdin = process.openStdin();
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const zipcodes = require("zipcodes");
+const bodyParser = require("body-parser");
+const { token } = require("./api");
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
-async function getYelp(zip){
-    
-    var location = await zipcodes.lookup(zip);
-    var latitude = location.latitude
-    var longitude = location.longitude
-    var radius = 5000 
-    var myUrl = `https://api.yelp.com/v3/businesses/search?term=restaurant&latitude=${latitude}&longitude=${longitude}&radius=${radius}&limit=50`
+async function getYelp(zip) {
+  var location = await zipcodes.lookup(zip);
+  var latitude = location.latitude;
+  var longitude = location.longitude;
+  var radius = 5000;
+  var myUrl = `https://api.yelp.com/v3/businesses/search?term=food&latitude=${latitude}&longitude=${longitude}&radius=${radius}&limit=50`;
 
-    const response = await axios({
-        method: "get",
-        url: myUrl,
-        headers:{ Authorization: `Bearer ${token}`}
-    })
+  const response = await axios({
+    method: "get",
+    url: myUrl,
+    headers: { Authorization: `Bearer ${token}` }
+  });
 
-    let restaurants = []
-    try{
-        for(let i = 0; i < response.data.total; i++){
-            restaurants.push(response.data.businesses[i].name)    
+  let restaurants = [];
 
-        }
+  try {
+    for (let i = 0; i < response.data.total; i++) {
+      restaurants.push(response.data.businesses[i].name);
     }
-    catch{}
-    let randomInt = getRandomInt(response.data.total-1)
+  } catch (e) {}
+  let randomInt = getRandomInt(response.data.total - 1);
 
-    try{
-        //console.log(restaurants[randomInt])
-        console.log(restaurants)
-    }
-    catch{
-        console.log("no places near you")
-    }
+  try {
+    console.log(restaurants[randomInt]);
+  } catch (e) {}
 }
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
+  max > 50 ? (max = 50) : (max = max);
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-//app.listen(port, ()=> console.log(`listening on port${port}...`))
+app.post("/restaurant", (req, res) => {
+  console.log(req.body.zip);
+  res.send(getYelp(req.body.zip));
+});
 
-process.stdout.write('enter your zipcode: ')
+app.listen(port, () => console.log(`listening on port${port}...`));
+
+/*process.stdout.write('enter your zipcode: ')
 
 stdin.addListener('data', text => {
   const consoleZip = text.toString().trim()
 
   stdin.pause() // stop reading
   getYelp(consoleZip);
-})
+})*/
